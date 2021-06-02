93bc931e4ac2f1b7879ac17f3a4f21f2a3e0990a
Omer Sabah <omerrj99@gmail.com>
2021-06-02 23:43:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const findMatching =(list, name) =>drivers.filter((driver) =>
      driver.toLowerCase() === name.toLowerCase());
 
 
-const fuzzyMatch =(list, partialName)=> {
-  let lengthOfName = partialName.length;
-  return list.filter(function (driverName) {
-    return driverName.slice(0, lengthOfName) === partialName;
-  });
-}
+const fuzzyMatch =(list, partialName)=> 
+
+   list.filter(function (driverName) 
+     driverName.slice(0, partialName.length) === partialName);
+
 
 const matchName=(drivers,name)=>{
    return drivers.filter((driver)=> {
